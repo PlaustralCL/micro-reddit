f674676709ebdb506619cda1085ad6dce992cabd
PlaustralCL <plaustralcl@gmail.com>
2022-01-22 22:04:12 -0500

    Create Post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts
+
   validates :name, presence: true
   validates :name, length: { minimum: 6 }
   validates :name, uniqueness: { case_sensitive: false }
